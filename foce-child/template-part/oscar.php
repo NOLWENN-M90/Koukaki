bc56fc4b1d917d5cbe4ab3b8f641c49e940cecbd
NOLWENN-M90 <125025120+NOLWENN-M90@users.noreply.github.com>
2023-08-23 09:58:05 +0200

    Add files via upload

---
+++ foce-child/template-part/oscar.php
@@ -0,0 +1,8 @@
+<section>
+    <div class="oscar reveal">
+        
+            <p class="text">Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</p>
+            <img class="osc" src="./wp-content/themes/foce-child/assets/images/court-metrage.png">
+        
+    </div>
+</section>
---
+++ foce-child/template-part/personnages.php
@@ -0,0 +1,23 @@
+<article class="reveal">
+    <img class="titr" src="./wp-content/themes/foce-child/assets/images/personnages/titre-personnage.png">
+    <swiper-container class="mySwiper" pagination="false" autoplay="true" loop="true" effect="coverflow" grab-cursor="true" centered-slides="false" slides-per-view="3" coverflow-effect-rotate="50" coverflow-effect-stretch="0" coverflow-effect-depth="100" coverflow-effect-modifier="1" coverflow-effect-slide-shadows="false">
+        <swiper-slide>
+            <img src="./wp-content/themes/foce-child/assets/images/personnages/Kawaneko.png" />
+        </swiper-slide>
+        <swiper-slide>
+            <img src="./wp-content/themes/foce-child/assets/images/personnages/Orenjiiro.png" />
+        </swiper-slide>
+        <swiper-slide>
+            <img src="./wp-content/themes/foce-child/assets/images/personnages/Pinku.png" />
+        </swiper-slide>
+        <swiper-slide>
+            <img src="./wp-content/themes/foce-child/assets/images/personnages/Tenshi.png" />
+        </swiper-slide>
+        <swiper-slide>
+            <img src="./wp-content/themes/foce-child/assets/images/personnages/Jaakuna.png" />
+
+        </swiper-slide>
+    </swiper-container>
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-element-bundle.min.js"></script>
+</article>
