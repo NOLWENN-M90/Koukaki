76dc7e680389bc99a9d667b8765935411a0be767
Nolwenn Méry <nolwenn.mery@gmail.com>
2023-09-08 15:32:19 +0200

    observer ok

--- foce-child/template-part/oscar.php
+++
@@ -1,8 +0,0 @@
-<section>
-    <div class="oscar reveal">
-        
-            <p class="text">Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</p>
-            <img class="osc" src="./wp-content/themes/foce-child/assets/images/court-metrage.png">
-        
-    </div>
-</section>
